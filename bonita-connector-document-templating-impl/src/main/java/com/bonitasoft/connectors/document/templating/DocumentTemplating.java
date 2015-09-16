2d430424c728da05c34f0df5c79269b538610ebd
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2015-09-16 16:22:07 +0200

    Add the connector

---
+++ bonita-connector-document-templating-impl/src/main/java/com/bonitasoft/connectors/document/templating/DocumentTemplating.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2015 Bonitasoft S.A.
+ * Bonitasoft is a trademark of BonitaSoft SA.
+ * This software file is BONITASOFT CONFIDENTIAL. Not For Distribution.
+ * For commercial licensing information, contact:
+ * Bonitasoft, 32 rue Gustave Eiffel – 38000 Grenoble
+ * or Bonitasoft US, 51 Federal Street, Suite 305, San Francisco, CA 94107
+ **/
+
+package com.bonitasoft.connectors.document.templating;
+
+import org.bonitasoft.engine.connector.AbstractConnector;
+import org.bonitasoft.engine.connector.ConnectorException;
+import org.bonitasoft.engine.connector.ConnectorValidationException;
+
+/**
+ * @author Baptiste Mesta
+ */
+public class DocumentTemplating extends AbstractConnector {
+    @Override
+    protected void executeBusinessLogic() throws ConnectorException {
+
+    }
+
+    @Override
+    public void validateInputParameters() throws ConnectorValidationException {
+
+    }
+}
