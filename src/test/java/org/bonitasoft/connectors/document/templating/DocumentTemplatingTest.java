de47b7e2ffcf944f5a4c38f9ea68dd114bf14198
Romain Bioteau <romain.bioteau@bonitasoft.com>
2020-06-24 16:49:32 +0200

    chore(oss) move to community
    * rename package
    * update license header

--- src/test/java/org/bonitasoft/connectors/document/templating/DocumentTemplatingTest.java
+++ src/test/java/org/bonitasoft/connectors/document/templating/DocumentTemplatingTest.java
@@ -1,16 +1,20 @@
 /**
- * Copyright (C) 2015 Bonitasoft S.A.
- * Bonitasoft is a trademark of BonitaSoft SA.
- * This software file is BONITASOFT CONFIDENTIAL. Not For Distribution.
- * For commercial licensing information, contact:
- * Bonitasoft, 32 rue Gustave Eiffel – 38000 Grenoble
- * or Bonitasoft US, 51 Federal Street, Suite 305, San Francisco, CA 94107
- **/
-package com.bonitasoft.connectors.document.templating;
-
-/**
- * @author Baptiste Mesta
+ * Copyright (C) 2020 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package org.bonitasoft.connectors.document.templating;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.anyLong;
@@ -46,7 +50,7 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import com.bonitasoft.engine.api.ProcessAPI;
+import org.bonitasoft.engine.api.ProcessAPI;
 
 import fr.opensagres.xdocreport.converter.ConverterTypeTo;
 import fr.opensagres.xdocreport.converter.ConverterTypeVia;
